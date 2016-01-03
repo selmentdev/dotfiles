338f99ffa709bdabcacf87512ef212b0416c0fd4
selmentdev <selmentdev@gmail.com>
2016-01-03 13:14:18 +0100

    Preety log headers

--- install.sh
+++ install.sh
@@ -2,24 +2,28 @@
 
 dotfiles="$HOME/.dotfiles"
 
+notify_stage() {
+    echo "\033[32m$1\033[0m"
+}
+
 link_file() {
     if [ ! -e $2 ] ; then
         ln -s $1 $2
     fi
 }
 
-echo "Installing/Updating dotfiles...\n"
+notify_stage "Installing/Updating dotfiles..."
 
 if [ ! -e $dotfiles/.git ]; then
-    echo "Cloning dotfiles\n"
+    notify_stage "Cloning dotfiles"
     git clone https://github.com/selmentdev/dotfiles.git $dotfiles
 else
-    echo "Updating dotfiles\n"
+    notify_stage "Updating dotfiles"
     cd $dotfiles && git pull
 fi
 
-echo "Setup ZSH\n"
+notify_stage "Setup ZSH"
 link_file $dotfiles/zprofile $HOME/.zprofile
 
-echo "Setup VIM\n"
+notify_stage "Setup VIM"
 link_file $dotfiles/vimrc $HOME/.vimrc
